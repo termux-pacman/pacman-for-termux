72698c9af6ef46601e6cde72a2f3004558feeedb
Maxython <mixython@gmail.com>
2021-05-16 11:26:19 +0300

    Added a script to install pacman.

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#Script for installing pacman.
+
+info(){
+echo -e "\033[1;36m\n# $1\033[0m"
+}
+
+commet(){
+echo -e "\033[0;37m# $1\033[0m"
+}
+
+error(){
+echo -e "\033[1;31m# $1\033[0m"
+}
+
+info 'System and package updates.'
+pkg update -y
+pkg upgrade -y
+
+info 'Installing packages.'
+pkg install build-essential asciidoc gpgme nettle -y
+
+info 'Directory creation.'
+for i in var/cache/ etc/pacman.d/
+do
+	dir="/data/data/com.termux/files/usr/$i"
+	if [[ -d $dir ]]; then
+		commet "Found: $dir."
+	else
+		mkdir $dir
+		commet "Create: $dir."
+	fi
+done
+
+info 'Installing pacman.'
+if [[ ! -d pacman ]]; then
+	error 'Not found: pacman.'
+	exit e
+fi
+cd pacman
+./configure --prefix=$PREFIX
+make
+make install
+
+info 'Done.'

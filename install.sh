8278e2ecbcbbfc50757aecf26388a833f7cdeace
Ivan Max <60917834+Maxython@users.noreply.github.com>
2021-05-30 11:20:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,12 +69,12 @@ settings_pacman(){
   wget --inet4-only http://mirror.archlinuxarm.org/aarch64/core/pacman-mirrorlist-20210307-1-any.pkg.tar.xz
   pacman -U pacman-mirrorlist-20210307-1-any.pkg.tar.xz --noconfirm
   rm pacman-mirrorlist-20210307-1-any.pkg.tar.xz
-  sed -i 's+RootDir     = /data/data/com.termux/files/usr/+RootDir     = /data/data/com.termux/files/+' $PREFIX/etc/pacman.conf
   sed -i 's/#this//' $PREFIX/etc/pacman.conf
 
   info 'Run pacman.'
   pacman -Syu
-  pacman -S filesystem --noconfirm
+  pacman -S iana-etc --noconfirm
+  sed -i 's+RootDir     = /data/data/com.termux/files/usr/+RootDir     = /data/data/com.termux/files/+' $PREFIX/etc/pacman.conf
 
   #info 'Setting up termux.'
   #file=$PREFIX/etc/bash.bashrc

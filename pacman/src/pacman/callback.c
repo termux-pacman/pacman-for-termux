64ffdd1be9653d6242707434d8b2383ee0af5ead
Ivan Max <60917834+Maxython@users.noreply.github.com>
2021-05-30 19:53:14 +0300

    Update callback.c

--- pacman/src/pacman/callback.c
+++ pacman/src/pacman/callback.c
@@ -373,8 +373,8 @@ void cb_question(alpm_question_t *question)
 			{
 				alpm_question_install_ignorepkg_t *q = &question->install_ignorepkg;
 				if(!config->op_s_downloadonly) {
-					//q->install = yesno(_("%s is in IgnorePkg. Install anyway?"),
-							alpm_pkg_get_name(q->pkg));
+					/*q->install = yesno(_("%s is in IgnorePkg. Install anyway?"),
+							alpm_pkg_get_name(q->pkg));*/
 					printf(_("The %s package is not supported. More details here https://github.com/Maxython/pacman-for-termux/wiki/Package-List.\n"),
 						alpm_pkg_get_name(q->pkg));
 					q->install = 0;

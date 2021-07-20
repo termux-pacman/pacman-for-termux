e5f408789a6a74f9e87f0e6e313910c51ade273c
Ivan Max <60917834+Maxython@users.noreply.github.com>
2021-07-20 22:27:34 +0300

    Update pack_list_unsup.h

--- pacman-6.0.0/src/pacman/pack_list_unsup.h
+++ pacman-6.0.0/src/pacman/pack_list_unsup.h
@@ -2,13 +2,35 @@
 
 struct str_pac
 {
-    char * p1;
-    char * p2;
-    char * p3;
+char * p1;
+char * p2;
+char * p3;
+char * p4;
+char * p5;
+char * p6;
+char * p7;
+char * p8;
+char * p9;
+char * p10;
+char * p11;
+char * p12;
+char * p13
 };
 
 struct str_pac list_pack = {
-    "pacman",
-    "bash",
-    "sudo"
+"pacman",
+"bash",
+"bzip2",
+"curl",
+"glibc",
+"gpgme",
+"libarchive",
+"libgpg-error",
+"libnghttp2",
+"libssh2",
+"libxml2",
+"ncurses",
+"openssl",
+"xz",
+"zlib"
 };

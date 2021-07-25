779ff6453cc1cc9bd934cb8031a9b3aa0628f57c
Maxython <mixython@gmail.com>
2021-07-25 23:40:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,11 +27,12 @@ install_packages(){
 
 install_pacman(){
   info 'Installing pacman.'
-  if [[ ! -d pacman ]]; then
+  dir=pacman-${type}
+  if [[ ! -d $dir ]]; then
     error 'Not found: pacman.'
     exit 2
   fi
-  cd pacman
+  cd $dir
   if [[ -z $1 || "$1" == "mes" ]]; then
     commet 'Running meson build.'
     meson build
@@ -64,43 +65,42 @@ install_pacman(){
 }
 
 settings_pacman(){
-  info 'Pacman settings.'
-  chmod 755 /data/data/com.termux/files/*
-  chmod 755 $PREFIX/*
-  wget --inet4-only http://mirror.archlinuxarm.org/aarch64/core/pacman-mirrorlist-20210307-1-any.pkg.tar.xz
-  pacman -U pacman-mirrorlist-20210307-1-any.pkg.tar.xz --noconfirm
-  rm pacman-mirrorlist-20210307-1-any.pkg.tar.xz
-  conf=$PREFIX/etc/pacman.conf
-  sed -i 's/#this//' $conf
-  sed -i 's+RootDir     = /data/data/com.termux/files/usr/+RootDir     = /data/data/com.termux/files/+' $conf
-  un="`uname -m`"
-  if [[ $un == "armv7l" ]]; then
-    sed -i 's/Architecture = auto/Architecture = armv7h/' $conf
-  fi
-
-  info 'Run pacman.'
-  pacman -Syu
-
-  info 'Setting up termux.'
-  for i in bin lib
-  do
-    if [[ ! -d /data/data/com.termux/files/$i ]]; then
-      ln -s $PREFIX/$i /data/data/com.termux/files/$i
+  if [[ "$type" == "arch" ]]; then
+    info 'Pacman settings.'
+    chmod 755 /data/data/com.termux/files/*
+    chmod 755 $PREFIX/*
+    wget --inet4-only http://mirror.archlinuxarm.org/aarch64/core/pacman-mirrorlist-20210307-1-any.pkg.tar.xz
+    pacman -U pacman-mirrorlist-20210307-1-any.pkg.tar.xz --noconfirm
+    rm pacman-mirrorlist-20210307-1-any.pkg.tar.xz
+    conf=$PREFIX/etc/pacman.conf
+    sed -i 's/#this//' $conf
+    sed -i 's+RootDir     = /data/data/com.termux/files/usr/+RootDir     = /data/data/com.termux/files/+' $conf
+    un="`uname -m`"
+    if [[ $un == "armv7l" ]]; then
+      sed -i 's/Architecture = auto/Architecture = armv7h/' $conf
     fi
-  done
-  chroot=$PREFIX/bin/termux-chroot
-  if [[ -z "`grep 'ARGS="$ARGS -b /sys:/sys"' $chroot`" ]]; then
-    sed -i '35a\ARGS="$ARGS -b /sys:/sys"' $chroot
+
+    info 'Run pacman.'
+    pacman -Syu
+
+    info 'Setting up termux.'
+    for i in bin lib
+    do
+      if [[ ! -d /data/data/com.termux/files/$i ]]; then
+        ln -s $PREFIX/$i /data/data/com.termux/files/$i
+      fi
+    done
+    chroot=$PREFIX/bin/termux-chroot
+    if [[ -z "`grep 'ARGS="$ARGS -b /sys:/sys"' $chroot`" ]]; then
+      sed -i '35a\ARGS="$ARGS -b /sys:/sys"' $chroot
+    fi
+    if [[ -z "`grep 'ARGS="$ARGS --link2symlink"' $chroot`" ]]; then
+      sed -i '35a\ARGS="$ARGS --link2symlink"' $chroot
+    fi
+    etc=$PREFIX/etc
+    rm $etc/profile
+    wget -P $etc --inet4-only https://raw.githubusercontent.com/Maxython/arch-packages-for-termux/main/profile/usr/etc/profile
   fi
-  if [[ -z "`grep 'ARGS="$ARGS --link2symlink"' $chroot`" ]]; then
-    sed -i '35a\ARGS="$ARGS --link2symlink"' $chroot
-  fi
-  if [[ -f $PREFIX/bin/login ]]; then
-    rm $PREFIX/bin/login
-  fi
-  etc=$PREFIX/etc
-  rm $etc/profile
-  wget -P $etc --inet4-only https://raw.githubusercontent.com/Maxython/arch-packages-for-termux/main/profile/usr/etc/profile
 
   info 'Removing deb packages.'
   apt-get purge clang python ninja bash-completion bsdtar -y
@@ -113,18 +113,44 @@ settings_pacman(){
 }
 
 settings2_pacman(){
-  info 'Start the second part of the setup.'
-  if [[ ! -d /bin || ! -d /lib ]]; then
-    error 'The /bin and /lib directory is not available.'
-    exit 2
+  if [[ "$type" == "arch" ]]; then
+    info 'Start the second part of the setup.'
+    if [[ ! -d /bin || ! -d /lib ]]; then
+      error 'The /bin and /lib directory is not available.'
+      exit 2
+    fi
+    pacman -Syu
+    pacman-key --init
+    pacman -S filesystem archlinuxarm-keyring --noconfirm --color=always --overwrite "*"  #archlinux-keyring
+    pacman-key --populate
+  else
+    info "You have pacman termux type, nothing to configure."
   fi
-  pacman -Syu
-  rm /bin
-  rm /lib
-  pacman-key --init
-  pacman -S filesystem archlinuxarm-keyring --noconfirm --color=always #archlinux-keyring
-  pacman-key --populate
 }
+
+if [[ ! -f selec.conf ]]; then
+  info "Select the pacman view, they are configured for a specific environment and for a specific type of command installation.  The script will also set up the environment for a particular kind of pacman."
+  PS3='Please enter your choice: '
+  options=("Arch" "Termux" "Quit")
+  select opt in "${options[@]}"
+  do
+    case $opt in
+        "Arch")
+          echo "type=arch" > selec.conf
+          break
+          ;;
+        "Termux")
+          echo "type=termux" > selec.conf
+          break
+          ;;
+        "Quit")
+          exit 0
+          ;;
+        *) echo "invalid option $REPLY";;
+    esac
+  done
+fi
+source selec.conf
 
 if [[ "$1" == "help" ]]; then
   info 'Help'

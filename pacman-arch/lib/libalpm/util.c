d9b1aef21beca00ba752ef9d1f81f8c4e0d0cb6a
Ivan Max <60917834+Maxython@users.noreply.github.com>
2021-08-13 01:51:36 +0300

    Update util.c

--- pacman-arch/lib/libalpm/util.c
+++ pacman-arch/lib/libalpm/util.c
@@ -658,37 +658,32 @@ int _alpm_run_chroot(alpm_handle_t *handle, const char *cmd, char *const argv[],
 		umask(0022);
 		_alpm_reset_signals();
 
-		/*if (strstr(cmd, "/bin/bashTermux") != NULL) {
-			execv(cmd, argv);
-		} else {*/
-			char* cmd2 = (char*)malloc(13 * sizeof(char));
-			sprintf(cmd2, "0");
-			for (int i = 0; 1; i++) {
-                		if (argv[i] != NULL) {
-					//printf("-> %s\n", argv[i]);
-                        		if (strcmp(cmd2, "0") == 0) {
-						if (strstr(argv[i], "/bin/sh") == NULL && strstr(argv[i], "/bin/bash") == NULL) {
-                                			if (strstr(argv[i], "while") == NULL && strstr(argv[i], ". ") == NULL) {
-								sprintf(cmd2, "asl '%s", argv[i]);
-							} else {
-								sprintf(cmd2, "%s", argv[i]);
-							}
+		char* cmd2 = (char*)malloc(13 * sizeof(char));
+		sprintf(cmd2, "0");
+		for (int i = 0; 1; i++) {
+                	if (argv[i] != NULL) {
+                        	if (strcmp(cmd2, "0") == 0) {
+					if (strstr(argv[i], "/bin/sh") == NULL && strstr(argv[i], "/bin/bash") == NULL) {
+                                		if (strstr(argv[i], "while") == NULL && strstr(argv[i], ". ") == NULL) {
+							sprintf(cmd2, "asl '%s", argv[i]);
 						} else {
-							i++;
+							sprintf(cmd2, "%s", argv[i]);
 						}
-                        		} else {
-                                		sprintf(cmd2, "%s %s", cmd2, argv[i]);
-                        		}
-                		} else {
-					if (strstr(cmd2, "'") != NULL) {
-						sprintf(cmd2, "%s'", cmd2);
+					} else {
+						i++;
 					}
-                        		break;
-                		}
-        		}
-			//printf("==> %s\n", cmd2);
-			execl("/data/data/com.termux/files/usr/bin/bashTermux", "bashTermux", "-c", cmd2, (char *) NULL);
-		//}
+                        	} else {
+                                	sprintf(cmd2, "%s %s", cmd2, argv[i]);
+                        	}
+                	} else {
+				if (strstr(cmd2, "'") != NULL) {
+					sprintf(cmd2, "%s'", cmd2);
+				}
+                        	break;
+                	}
+        	}
+		printf("==> %s\n", cmd2);
+		execl("/data/data/com.termux/files/usr/bin/bashTermux", "bashTermux", "-c", cmd2, (char *) NULL);
 		/* execv only returns if there was an error */
 		fprintf(stderr, _("call to execv failed (%s)\n"), strerror(errno));
 		exit(1);

b448389dc3ebb14e6dee3ddeff65ceafc7acff91
Ivan Max <60917834+Maxython@users.noreply.github.com>
2021-08-18 12:50:34 +0300

    Update pack_list_unsup.h

--- pacman-arch/src/pacman/pack_list_unsup.h
+++ pacman-arch/src/pacman/pack_list_unsup.h
@@ -2,19 +2,6 @@
 
 const char* list_pack[] = {
 "pacman",
-"bzip2",
-"curl",
-"glibc",
-"gpgme",
-"libarchive",
-"libgpg-error",
-"libnghttp",
-"libssh2",
-"libxml2",
-"ncurses",
-"openssl",
-"xz",
-"zlib",
 "talloc",
 NULL
 };
